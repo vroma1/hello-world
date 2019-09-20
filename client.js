36c249a50ce962145bc7227485bd2a13f82c4d69
vroma1 <55601405+vroma1@users.noreply.github.com>
2019-09-20 23:10:07 +0300

    Update client.js

--- client.js
+++ client.js
@@ -8,8 +8,6 @@ function hasUserMedia()
  
 if (hasUserMedia()) 
 { 
-   alert("hasUserMedia"); 
-
    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
       || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 		
